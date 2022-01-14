08d4b7b945e7a85b0aa8a6961b71077808e7dac4
artpongsakorn <96404204+artpongsakorn@users.noreply.github.com>
2022-01-14 16:23:16 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'K4hg22zZ6Ke6MrxOPwOdArq6HvOZe/bjW1xX/iI01/SuOzGCmpftJAdgUog0hZEwVy4TAYNd0BQmzM6GlISCG4Od4iFpqZS8wXv0TMvkXdNxEULXnNy9/hoXLY4Nd2nZfe9UPkipRjVtChsm4PXEegdB04t89/1O/w1cDnyilFU=';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
